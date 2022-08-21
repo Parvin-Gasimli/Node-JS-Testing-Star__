3ab3d70cb69d15f66c32f1ff103ec9dcbfb6a81d
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2022-08-21 22:21:20 +0400

    Sonme Changes and some checking datra

--- controller/Tasks.js
+++ controller/Tasks.js
@@ -1,24 +1,26 @@
-const Task =require("../starter/models/Task")
+const Task = require("../starter/models/Task");
 
+const getAllTasks = (req, res) => {
+  res.send("all items for you");
+};
 
-const getAllTasks=(req,res)=>{
-    res.send("all items for you")
-}
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({msg:error})
+  }
+};
 
-const createTask= async(req,res)=>{
-    const task= await Task.create(req.body)
-    res.status(201).json({task})
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+const updateTask = (req, res) => {
+  res.send("update Task");
+};
+const deleteTask = (req, res) => {
+  res.send("delete Task");
+};
 
-}
-
-const getTask=(req,res)=>{
-    res.json({id:req.params.id})
-}
-const updateTask=(req,res)=>{
-    res.send("update Task")
-}
-const deleteTask=(req,res)=>{
-    res.send("delete Task")
-}
-
-module.exports={getAllTasks,createTask,getTask,deleteTask,updateTask}
+module.exports = { getAllTasks, createTask, getTask, deleteTask, updateTask };
